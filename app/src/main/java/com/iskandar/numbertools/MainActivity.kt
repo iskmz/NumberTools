2ca346d6ee61b4de797e7ae841a829168ea6dcd4
iskmz <iskandarmazzawi@gmail.com>
2019-08-08 19:21:13 +0300

    finished Base Conversions Code

--- app/src/main/java/com/iskandar/numbertools/MainActivity.kt
+++ app/src/main/java/com/iskandar/numbertools/MainActivity.kt
@@ -3,9 +3,12 @@ package com.iskandar.numbertools
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var input = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +18,56 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
+
     private fun setListeners() {
 
         btnInfo.setOnClickListener { showInfoDialog() }
+
+        // base conversions //
+        btnBinary.setOnClickListener {
+            if(!inputOK()) return@setOnClickListener
+            showResultDialog(input.toLong(),"Conversion To Binary", input.toLong().toString(2))
+        }
+        btnOctal.setOnClickListener {
+            if(!inputOK()) return@setOnClickListener
+            showResultDialog(input.toLong(),"Conversion To Octal",input.toLong().toString(8))
+        }
+        btnHexa.setOnClickListener {
+            if(!inputOK()) return@setOnClickListener
+            showResultDialog(input.toLong(),"Conversion To Hexadecimal",input.toLong().toString(16))
+        }
+
+
+        // manipulations / calculations //
+        btnReverse.setOnClickListener {  }
+        btnDivisors.setOnClickListener {  }
+        btnSum.setOnClickListener {  }
+
+
+        // convert to text //
+        btnDigByDigReading.setOnClickListener {  }
+        btnNaturalReading.setOnClickListener {  }
+    }
+
+    private fun showResultDialog(input: Long, oprName: String, result:String)
+    {
+
+        val res = AlertDialog.Builder(this@MainActivity)
+            .setTitle("Result Window!")
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+            .setMessage("Original Number: $input \n\nOperation: $oprName \n\nResult: $result")
+            .create()
+        res.setCanceledOnTouchOutside(false)
+        res.show()
+    }
+
+    private fun inputOK(): Boolean {
+        input = txtInputNum.text.toString()
+        if(input.isEmpty()) {
+            Toast.makeText(this@MainActivity,"Input must NOT be empty!",Toast.LENGTH_LONG).show()
+            return false
+        }
+        return true
     }
 
 
