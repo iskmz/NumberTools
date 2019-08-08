fd8778542281986d6bcc46beca335c61b17d9963
iskmz <iskandarmazzawi@gmail.com>
2019-08-08 18:49:52 +0300

    basic layout design , done!

--- app/src/main/java/com/iskandar/numbertools/MainActivity.kt
+++ app/src/main/java/com/iskandar/numbertools/MainActivity.kt
@@ -2,11 +2,35 @@ package com.iskandar.numbertools
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setListeners()
     }
+
+    private fun setListeners() {
+
+        btnInfo.setOnClickListener { showInfoDialog() }
+    }
+
+
+    private fun showInfoDialog() {
+        val info = AlertDialog.Builder(this@MainActivity)
+            .setTitle("Number Tools")
+            .setMessage("by Iskandar Mazzawi \u00a9")
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss()  }
+            .setNegativeButton("Exit App.") { _,_ -> finish() }
+            .setIcon(R.drawable.ic_info_orange)
+            .create()
+        info.setCanceledOnTouchOutside(false)
+        info.show()
+    }
+
 }
