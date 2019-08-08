d21cdc04dc48f541b1246388ca54b4e38b2b28bc
iskmz <iskandarmazzawi@gmail.com>
2019-08-08 20:41:02 +0300

    done with manipulations code

--- app/src/main/java/com/iskandar/numbertools/MainActivity.kt
+++ app/src/main/java/com/iskandar/numbertools/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,21 +39,29 @@ class MainActivity : AppCompatActivity() {
             showResultDialog(input.toLong(),"Conversion To Hexadecimal",input.toLong().toString(16))
         }
 
-
         // manipulations / calculations //
-        btnReverse.setOnClickListener {  }
-        btnDivisors.setOnClickListener {  }
-        btnSum.setOnClickListener {  }
-
+        btnReverse.setOnClickListener {
+            if(!inputOK()) return@setOnClickListener
+            showResultDialog(input.toLong(),"Reverse Digits Order", getReversedDigits())
+        }
+        btnDivisors.setOnClickListener {
+            if(!inputOK()) return@setOnClickListener
+            showResultDialog(input.toLong(),"Find Proper Divisors", getDivisors())
+        }
+        btnSum.setOnClickListener {
+            if(!inputOK()) return@setOnClickListener
+            showResultDialog(input.toLong(),"Calculate Sum of Digits", calcDigitSum())
+        }
 
         // convert to text //
         btnDigByDigReading.setOnClickListener {  }
         btnNaturalReading.setOnClickListener {  }
     }
 
-    private fun showResultDialog(input: Long, oprName: String, result:String)
-    {
 
+/////////////////////////////////   general utility functions ////////////////////////////////////////////////////
+
+    private fun showResultDialog(input: Long, oprName: String, result:String) {
         val res = AlertDialog.Builder(this@MainActivity)
             .setTitle("Result Window!")
             .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
@@ -70,7 +80,6 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-
     private fun showInfoDialog() {
         val info = AlertDialog.Builder(this@MainActivity)
             .setTitle("Number Tools")
@@ -83,4 +92,47 @@ class MainActivity : AppCompatActivity() {
         info.show()
     }
 
+
+/////////////////////////////////   manipulations / calculations  functions  /////////////////////////////////////
+
+
+    private fun getReversedDigits(): String {
+        var tmp = input.toLong()
+        var revTmp = 0L
+        while(tmp>0){ revTmp= revTmp*10+tmp%10; tmp/=10 }
+        return  revTmp.toString()
+    }
+
+
+    private fun getDivisors(): String {
+
+        val tmp = input.toLong()
+
+        if (tmp==0.toLong()) return "No Divisors"
+        if(tmp==1.toLong()) return "1"
+        if(tmp > (10.0.pow(9.0)).toLong()) return "\n<<OUT-OF-RANGE-LIMIT>>\nPlease enter a number less than One-Billion!"
+
+        var divListStart="1"
+        var divListEnd=""
+        var checkLimit = sqrt(tmp.toDouble())
+
+        for(i in 2..checkLimit.toLong())
+        {
+            if(tmp%i==0.toLong())
+            {
+                if(i==tmp/i) { divListStart += " , $i" }
+                else { divListStart += " , $i"; divListEnd = " , ${tmp/i}$divListEnd" }
+            }
+        }
+        return (divListStart+divListEnd)
+    }
+
+
+    private fun calcDigitSum(): String {
+
+        var tmp = input.toLong()
+        var sum = 0
+        while (tmp>0) { sum += (tmp%10).toInt(); tmp/=10 }
+        return sum.toString()
+    }
 }
